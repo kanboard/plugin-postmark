db9ac4fa124a252f3cee8e068c2bbd1d9acbb726
Frederic Guillot <fred@kanboard.net>
2016-07-31 10:22:55 -0400

    Fix typo for icon URL

--- Template/integration.php
+++ Template/integration.php
@@ -1,4 +1,4 @@
-<h3><img src="<?= $this->url->dir() ?>plugins/postmark/postmark-icon.png"/>&nbsp;Postmark</h3>
+<h3><img src="<?= $this->url->dir() ?>plugins/Postmark/postmark-icon.png"/>&nbsp;Postmark</h3>
 <div class="listing">
     <input type="text" class="auto-select" readonly="readonly" value="<?= $this->url->href('WebhookController', 'receiver', array('plugin' => 'postmark', 'token' => $values['webhook_token']), false, '', true) ?>"/>
 
@@ -6,7 +6,7 @@
     <?= $this->form->text('postmark_api_token', $values) ?>
 
     <p class="form-help"><a href="https://kanboard.net/plugin/postmark" target="_blank"><?= t('Help on Postmark integration') ?></a></p>
-    
+
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue">
     </div>
