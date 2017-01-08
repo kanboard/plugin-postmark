5413ccf89881ee3cb5e53fa10d1fb697e41cb92d
Frederic Guillot <fred@kanboard.net>
2017-01-08 18:35:10 -0500

    Rename CSS class

--- Template/integration.php
+++ Template/integration.php
@@ -1,5 +1,5 @@
 <h3><img src="<?= $this->url->dir() ?>plugins/Postmark/postmark-icon.png"/>&nbsp;Postmark</h3>
-<div class="listing">
+<div class="panel">
     <input type="text" class="auto-select" readonly="readonly" value="<?= $this->url->href('WebhookController', 'receiver', array('plugin' => 'postmark', 'token' => $values['webhook_token']), false, '', true) ?>"/>
 
     <?= $this->form->label(t('Postmark API token'), 'postmark_api_token') ?>
